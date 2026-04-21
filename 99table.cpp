aa3e9ad2c69777430d36f901d8e3392536f8c62b
Joshua2892 <s1130110@dmhs.kh.edu.tw>
2026-04-21 11:32:17 +0800

    Create 99table.cpp

---
+++ 99table.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : apcs.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	for(int i=1;i<=9;i++){
+		for(int j=1;j<=3;j++){
+			cout<<j<<"*"<<i<<"="<<j*i<<" ";
+		}
+		cout<<"\n";
+	}
+	cout<<"--------\n";
+	for(int i=1;i<=9;i++){
+			for(int j=4;j<=4;j++){
+				cout<<j<<"*"<<i<<"="<<j*i<<" ";
+			}
+			cout<<"\n";
+		}
+	cout<<"--------\n";
+	for(int i=1;i<=9;i++){
+			for(int j=7;j<=9;j++){
+				cout<<j<<"*"<<i<<"="<<j*i<<" ";
+			}
+			cout<<"\n";
+		}
+	return 0;
+}
